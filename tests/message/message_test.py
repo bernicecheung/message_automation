e5257929dd1b99a7ef13887e0071487d32db4443
Paul Novak <pnovak@alumni.caltech.edu>
2021-02-10 11:54:51 -0800

    Use permutations strategy

--- tests/message/message_test.py
+++ tests/message/message_test.py
@@ -9,14 +9,12 @@ from src.enums import Condition, CodedValues
 
 # Test that every combination of Condition and CodedValues has enough messages
 @given(c=st.sampled_from(Condition),
-       v1=st.sampled_from(CodedValues),
-       v2=st.sampled_from(CodedValues),
-       v3=st.sampled_from(CodedValues))
-def test_enough_messages(c, v1, v2, v3):
+       v=st.permutations(CodedValues).map(lambda x: x[:3]))
+def test_enough_messages(c, v):
     shared_datadir = Path.cwd() / 'message' / 'data'
     messages = MessageLibrary(path=str(shared_datadir / 'messages.csv'))
     num_required_messages = 28 * (MESSAGES_PER_DAY_1 + MESSAGES_PER_DAY_2)
 
-    condition_messages = messages.get_messages_by_condition(c, [v1, v2, v3], num_required_messages)
+    condition_messages = messages.get_messages_by_condition(c, v, num_required_messages)
 
     assert len(condition_messages) >= num_required_messages
